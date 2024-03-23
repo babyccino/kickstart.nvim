f7861aefe1490769e13bd9d6741bbb386e9291bc
Gus <gus.ryan163@gmail.com>
2024-03-23 16:07:25 +0000

    refactor: put large plugin configs in their own files

--- init.lua
+++ init.lua
@@ -13,16 +13,3 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 require 'custom'
--- [[ Configure and install plugins ]]
---
---  To check the current status of your plugins, run
---    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
---
--- NOTE: Here is where you install your plugins.
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
