ff8957ad10d905fb4e030f921bf1e598b60d3216
Gus <gus.ryan163@gmail.com>
2024-03-23 23:21:42 +0000

    use prime's scroll bindings

--- lua/custom/remap.lua
+++ lua/custom/remap.lua
@@ -83,8 +83,9 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagn
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
--- use semicolon instead of colon for command mode
-vim.keymap.set('n', ';', ':', {})
+-- prime's scroll keymap
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { noremap = true })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { noremap = true })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
