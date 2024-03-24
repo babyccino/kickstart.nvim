880a31776799b309225c3ac63c748c961bc2f80b
Gus <gus.ryan163@gmail.com>
2024-03-24 03:40:50 +0000

    refactor: use null ls, setup tsserver manually

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -5,7 +5,8 @@ local lspconfig = { -- LSP Configuration & Plugins
     'williamboman/mason.nvim',
     'williamboman/mason-lspconfig.nvim',
     'WhoIsSethDaniel/mason-tool-installer.nvim',
-
+    'jose-elias-alvarez/null-ls.nvim',
+    'jose-elias-alvarez/typescript.nvim',
     -- Useful status updates for LSP.
     -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
     { 'j-hui/fidget.nvim', opts = {} },
@@ -15,35 +16,100 @@ local lspconfig = { -- LSP Configuration & Plugins
     { 'folke/neodev.nvim', opts = {} },
   },
   config = function()
-    -- Brief aside: **What is LSP?**
-    --
-    -- LSP is an initialism you've probably heard, but might not understand what it is.
-    --
-    -- LSP stands for Language Server Protocol. It's a protocol that helps editors
-    -- and language tooling communicate in a standardized fashion.
-    --
-    -- In general, you have a "server" which is some tool built to understand a particular
-    -- language (such as `gopls`, `lua_ls`, `rust_analyzer`, etc.). These Language Servers
-    -- (sometimes called LSP servers, but that's kind of like ATM Machine) are standalone
-    -- processes that communicate with some "client" - in this case, Neovim!
-    --
-    -- LSP provides Neovim with features like:
-    --  - Go to definition
-    --  - Find references
-    --  - Autocompletion
-    --  - Symbol Search
-    --  - and more!
-    --
-    -- Thus, Language Servers are external tools that must be installed separately from
-    -- Neovim. This is where `mason` and related plugins come into play.
-    --
-    -- If you're wondering about lsp vs treesitter, you can check out the wonderfully
-    -- and elegantly composed help section, `:help lsp-vs-treesitter`
+    local lspconfig = require 'lspconfig'
+    local typescript = require 'typescript'
+    local null_ls = require 'null-ls'
 
-    --  This function gets run when an LSP attaches to a particular buffer.
-    --    That is to say, every time a new file is opened that is associated with
-    --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
-    --    function will be executed to configure the current buffer
+    -- Setup everything on lsp attach
+
+    null_ls.setup {
+      sources = {
+        -- JS/TS formatting
+        null_ls.builtins.formatting.prettierd,
+        -- Spell check
+        --null_ls.builtins.diagnostics.cspell,
+        -- Python formatting
+        null_ls.builtins.formatting.autopep8.with {
+          extra_args = {
+            'max-line-length',
+            '79',
+            'aggressive',
+            'aggressive',
+            'aggressive',
+          },
+        },
+        -- Lua formatting
+        null_ls.builtins.formatting.stylua,
+      },
+      root_dir = lspconfig.util.root_pattern('yarn.lock', 'lerna.json', '.git'),
+      --debug = true,
+      log = { enabled = true, level = 'trace' },
+    }
+    local handle_lsp = function(opts)
+      return opts
+    end
+
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+    local capabilitiesWithoutFomatting = vim.lsp.protocol.make_client_capabilities()
+    capabilitiesWithoutFomatting.textDocument.formatting = false
+    capabilitiesWithoutFomatting.textDocument.rangeFormatting = false
+    capabilitiesWithoutFomatting.textDocument.range_formatting = false
+
+    -- Tsserver setup
+    typescript.setup {
+      disable_commands = false, -- prevent the plugin from creating Vim commands
+      debug = false, -- enable debug logging for commands
+      go_to_source_definition = {
+        fallback = true, -- fall back to standard LSP definition on failure
+      },
+      server = handle_lsp {
+        root_dir = lspconfig.util.root_pattern('yarn.lock', 'lerna.json', '.git'),
+        on_attach = function(client, bufnr)
+          client.server_capabilities.document_formatting = false
+          client.server_capabilities.document_range_formatting = false
+
+          --ts_utils_attach(client)
+        end,
+        settings = {
+          documentFormatting = false,
+          typescript = {
+            inlayHints = {
+              includeInlayParameterNameHints = 'all',
+              includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayVariableTypeHints = true,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayEnumMemberValueHints = true,
+            },
+          },
+        },
+        init_options = {
+          hostInfo = 'neovim',
+          maxTsServerMemory = '8192',
+          preferences = { quotePreference = 'single', allowIncompleteCompletions = false },
+          --tsserver = {
+          --path = "/home/armeeh/.config/yarn/global/node_modules/typescript/lib/tsserver.js",
+          --},
+        },
+        capabilities = capabilitiesWithoutFomatting,
+      },
+    }
+
+    --local luadev = require("lua-dev").setup({
+    --lspconfig = {
+    --cmd = {
+    --"/usr/bin/lua-language-server",
+    --"-E",
+    --"/usr/share/lua-language-server/main.lua",
+    --},
+    --on_attach = on_attach,
+    --capabilities = capabilities,
+    --},
+    --})
+    --lspconfig.sumneko_lua.setup(handle_lsp(luadev))
+
     vim.api.nvim_create_autocmd('LspAttach', {
       group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
       callback = function(event)
@@ -121,7 +187,6 @@ local lspconfig = { -- LSP Configuration & Plugins
     --  By default, Neovim doesn't support everything that is in the LSP specification.
     --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
     --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
     -- Enable the following language servers
@@ -133,43 +198,20 @@ local lspconfig = { -- LSP Configuration & Plugins
     --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
     --  - settings (table): Override the default settings passed when initializing the server.
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-    local servers = {
-      -- clangd = {},
-      gopls = {},
-      -- pyright = {},
-      -- rust_analyzer = {},
-      -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-      --
-      -- Some languages (like typescript) have entire language plugins that can be useful:
-      --    https://github.com/pmizio/typescript-tools.nvim
-      --
-      -- But for many setups, the LSP (`tsserver`) will work just fine
-      -- tsserver = {},
-      -- tsserver = {},
-      --
 
+    local servers = {
+      gopls = {},
       lua_ls = {
-        -- cmd = {...},
-        -- filetypes = { ...},
-        -- capabilities = {},
         settings = {
           Lua = {
             completion = {
               callSnippet = 'Replace',
             },
-            -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-            -- diagnostics = { disable = { 'missing-fields' } },
           },
         },
       },
     }
 
-    -- Ensure the servers and tools above are installed
-    --  To check the current status of installed tools and/or manually install
-    --  other tools, you can run
-    --    :Mason
-    --
-    --  You can press `g?` for help in this menu.
     require('mason').setup()
 
     -- You can add other tools here that you want Mason to install
@@ -184,9 +226,15 @@ local lspconfig = { -- LSP Configuration & Plugins
       handlers = {
         function(server_name)
           local server = servers[server_name] or {}
+
+          -- tsserver is setup manually
+          if server_name == 'tsserver' then
+            return
+          end
           -- This handles overriding only values explicitly passed
           -- by the server configuration above. Useful when disabling
           -- certain features of an LSP (for example, turning off formatting for tsserver)
+
           server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
           require('lspconfig')[server_name].setup(server)
         end,
